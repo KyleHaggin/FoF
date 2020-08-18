fb88a1e5bb055b14ec21f24583e151d269a121f9
KyleHaggin <KyleHaggin@users.noreply.github.com>
2020-08-18 14:13:31 -0700

    able to load summoner information from RIOT API

--- KMS_LoL/riot_api.py
+++ KMS_LoL/riot_api.py
@@ -1,1 +1,16 @@
 import os
+import riotwatcher as rw
+import pandas as pd
+from dotenv import load_dotenv
+
+# Load .env
+load_dotenv(verbose=True)
+
+# Find/Create global variables
+api_key = os.getenv('API_KEY')
+watcher = rw.LolWatcher(api_key)
+region = 'na1'
+
+# Create summoner information to test with. Julz in this case.
+test_summoner = watcher.summoner.by_name(region, 'Rârgh')
+print(test_summoner)

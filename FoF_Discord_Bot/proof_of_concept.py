9ed03dbeac2cac734952df92386825be1660cf44
KyleHaggin <KyleHaggin@users.noreply.github.com>
2020-12-30 04:32:27 -0800

    Most basic proof of concept for Discord bot.

---
+++ FoF_Discord_Bot/proof_of_concept.py
@@ -0,0 +1,34 @@
+import discord
+from discord.ext import commands
+import os
+from dotenv import load_dotenv
+
+# Load .env
+load_dotenv(verbose=True)
+
+# Get tokens and credentials from .env
+token = os.getenv('FoF_BOT_TOKEN')
+
+# Set discord client command prefix
+# (what people type as a activation for a bot command)
+client = commands.Bot(command_prefix='!')
+
+
+# On ready event (brings bot online in the server)
+@client.event
+async def on_ready():
+    print('Bot is Ready')
+
+
+# Create proof of concept message return for the bot
+@client.event
+async def on_message(message):
+    # Set bot to ignore messages from itself
+    if message.author == client.user:
+        return
+
+    # If bot sees a message that starts with Hello, reply 'Hello!' back
+    if message.content.startswith('Hello'):
+        await message.channel.send('Hello!')
+
+client.run(token)
